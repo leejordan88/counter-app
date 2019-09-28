ca7e8437b9d194d4c59309540b54d8a114c8369c
leejoonsung <leejs198@gmail.com>
2019-09-28 12:20:58 +0900

    js update

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('hello world');
+const counterApp = {
+  count: 0,
+  increment: function() {
+    this.count = this.count + 1;
+  },
+  decrement: function() {
+    this.count = this.count - 1;
+  },
+  getCount: function() {
+    return this.count;
+  },
+};
+
+const decrement = document.querySelector('.decrement-btn');
+const increment = document.querySelector('.increment-btn');
+const counter = document.querySelector('.counter-number');
+
+decrement.addEventListener('click', function() {
+  counterApp.decrement();
+  counter.innerHTML = counterApp.getCount();
+});
+
+increment.addEventListener('click', function() {
+  counterApp.increment();
+  counter.innerHTML = counterApp.getCount();
+});
